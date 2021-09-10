32366c4046afcd37d66921c2fb2d14c11df19cf4
Julian <31627984+juli6464@users.noreply.github.com>
2021-09-10 10:18:44 -0500

    Add files via upload
    Archivo error 404.php

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header() ?>
+
+<main class='container'>
+    <div class="pagina404 my-5">
+        <h1>Error 404</h1>
+        <h2>Haz<a href="<?php echo home_url() ?>"> click aquí</a> para volver al inicio</h2>
+    </div>
+</main>
+
+
+<?php get_footer() ?>
